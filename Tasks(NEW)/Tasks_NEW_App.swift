64f2645e03d47ddda697d8444f4f8e90ff3b51f8
Chirag Arora <arora2002chirag@gmail.com>
2022-12-27 02:46:13 +0530

    Initial Commit

---
+++ Tasks(NEW)/Tasks_NEW_App.swift
@@ -0,0 +1,17 @@
+//
+//  Tasks_NEW_App.swift
+//  Tasks(NEW)
+//
+//  Created by chirag arora on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Tasks_NEW_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
